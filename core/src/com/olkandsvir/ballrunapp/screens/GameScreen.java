d96ef1617b062f3babf24b5a00338c66eba6cb44
Vsevolod Svirin <bbcgit73195>
2016-04-15 18:46:38 +0300

    Adding comments

--- core/src/com/olkandsvir/ballrunapp/screens/GameScreen.java
+++ core/src/com/olkandsvir/ballrunapp/screens/GameScreen.java
@@ -32,7 +32,7 @@ public class GameScreen implements Screen {
     @Override
     public void render(float delta) {
 
-        //контроль за изменением fps (пока придется поверить на слово)
+        //контроль за изменением fps
         runTime += delta;
 
         //запускаем метод render из GameRenderer
